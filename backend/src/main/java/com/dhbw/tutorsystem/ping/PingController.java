6e0ea1ba521ec26a8995a92d3176ab0529e6c618
Robin <rbae@web.de>
2022-02-22 10:32:35 +0100

    fix error and enable front-back connection

--- backend/src/main/java/com/dhbw/tutorsystem/ping/PingController.java
+++ backend/src/main/java/com/dhbw/tutorsystem/ping/PingController.java
@@ -1,10 +1,12 @@
 package com.dhbw.tutorsystem.ping;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PingController {
  
     @GetMapping("/ping")
---
+++ backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.dhbw.tutorsystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
