2cff92ece1a120903dd0d9aad96ba8d9377f1a27
FinnHuelsbusch <f.huelsbusch@outlook.de>
2022-02-22 08:47:25 +0100

    initial commit

---
+++ backend/src/main/java/com/dhbw/tutorsystem/TutorsystemApplication.java
@@ -0,0 +1,13 @@
+package com.dhbw.tutorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dhbw/tutorsystem/TutorsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhbw.tutorsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
