33fca4d0a2dac07e76d67a11484437f99c6443c2
Robin <rbae@web.de>
2022-03-01 13:57:21 +0100

    add mail templating engine

--- backend/src/main/java/com/dhbw/tutorsystem/mails/EmailSenderService.java
+++ backend/src/main/java/com/dhbw/tutorsystem/mails/EmailSenderService.java
@@ -1,17 +1,21 @@
 package com.dhbw.tutorsystem.mails;
 
 import java.util.Base64;
+import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Service
 public class EmailSenderService {
@@ -40,23 +44,21 @@ public class EmailSenderService {
         return javaMailSender.createMimeMessage();
     }
 
+    @Autowired
+    private SpringTemplateEngine thymleafTemplateEngine;
+
+
     public void sendRegistrationMail(String mailTo, String hashBase64) throws MessagingException {
+        Context thymeleafContext = new Context();
+        String linkUrl = frontendUrl + "/verify?h=" + hashBase64 + "&e=" + mailTo;
+        thymeleafContext.setVariable("link", linkUrl);
+        thymeleafContext.setVariable("dhbw-logo", "dhbw-logo.png");
+        String htmlBody = thymleafTemplateEngine.process("registrationInfo.html", thymeleafContext);
+
         MimeMessageHelper helper = new MimeMessageHelper(getMimeMessage(), true, "utf-8");
         helper.setTo(mailTo);
         helper.setSubject("Registrierung Tutorensystem");
-        String linkUrl = frontendUrl + "/verify?h=" + hashBase64 + "&e=" + mailTo;
-        helper.setText(
-                "<p>Hallo,</p>" +
-                        "<p>Sie haben sich im Tutorensystem registriert.</p>" +
-                        "<p>Um ihr Konto zu aktivieren, klicken Sie bitte auf den nachfolgenden Link: " +
-                        "<a href='" +
-                        linkUrl +
-                        "''>" +
-                        linkUrl +
-                        "</a></p>" +
-                        "<p>Mit freundlichen Grüßen</p>" +
-                        "<p>Ihr Tutorsystem</p>",
-                true);
+        helper.setText(htmlBody, true);
         sendMimeMessage(helper.getMimeMessage());
     }
 }
--- backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfiguration.java
+++ backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfiguration.java
@@ -1,8 +1,13 @@
 package com.dhbw.tutorsystem.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 public class WebSecurityConfiguration implements WebMvcConfigurer {
@@ -11,4 +16,24 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Bean
+    public ClassLoaderTemplateResolver customTemplateResolver() {
+        ClassLoaderTemplateResolver configurer = new ClassLoaderTemplateResolver();
+        configurer.setPrefix("mail-templates/");
+        configurer.setSuffix(".html");
+        configurer.setTemplateMode(TemplateMode.HTML);
+        configurer.setCharacterEncoding("UTF-8");
+        configurer.setOrder(0); // this is important. This way spring boot will listen to both places 0 and 1
+        configurer.setCheckExistence(true);
+        return configurer;
+    }
+
+    // @Bean
+    // public SpringTemplateEngine thymeleafTemplateEngine(ITemplateResolver templateResolver) {
+    //     SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    //     templateEngine.setTemplateResolver(templateResolver);
+    //     templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+    //     return templateEngine;
+    // }
 }
