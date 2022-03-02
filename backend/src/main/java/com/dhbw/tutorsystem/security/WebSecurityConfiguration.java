04c11498c88cd29e9f791705ea2355df562a17d1
FinnHuelsbusch <f.huelsbusch@outlook.de>
2022-03-02 19:53:43 +0100

    updated security for put

--- backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfiguration.java
+++ backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfiguration.java
@@ -9,6 +9,6 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("POST","PUT","GET");
     }
 }
