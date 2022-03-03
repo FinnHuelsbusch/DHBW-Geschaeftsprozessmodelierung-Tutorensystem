96a0d5d135df5b0b92e7c97539f48c8ff52a5fd8
Robin <rbae@web.de>
2022-03-03 18:34:06 +0100

    improve error handling

--- backend/src/main/java/com/dhbw/tutorsystem/exception/RestResponseEntityExceptionHandler.java
+++ backend/src/main/java/com/dhbw/tutorsystem/exception/RestResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.dhbw.tutorsystem.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +11,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = { TSBaseException.class })
-    protected ResponseEntity<TSExceptionResponse> handleConflict(TSBaseException tsBaseException) {
-        TSExceptionResponse response = new TSExceptionResponse(LocalDateTime.now(), tsBaseException.getStatus().value(),
-                tsBaseException.getErrorCode(), tsBaseException.getMessage());
-        return new ResponseEntity<TSExceptionResponse>(response, tsBaseException.getStatus());
+    // catch all exceptions and put them into defined error object as JSON response
+    @ExceptionHandler(value = { Exception.class })
+    protected ResponseEntity<TSExceptionResponse> handleConflict(Exception exception) {
+        TSExceptionResponse response;
+        if (exception instanceof TSBaseException) {
+            TSBaseException tsBaseException = (TSBaseException) exception;
+            response = new TSExceptionResponse(LocalDateTime.now(), tsBaseException.getStatus().value(),
+                    tsBaseException.getErrorCode(), tsBaseException.getMessage());
+        } else {
+            // unknown exception, do not give too many details
+            response = new TSExceptionResponse(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    TSErrorCode.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+        return new ResponseEntity<TSExceptionResponse>(response, HttpStatus.valueOf(response.getStatus()));
     }
 
 }
