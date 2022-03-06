1b2f91478a88b1dc73601d3b901973f65edc2c01
FinnHuelsbusch <f.huelsbusch@outlook.de>
2022-03-06 19:18:06 +0100

    added new route to permitted Routes

--- backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfig.java
+++ backend/src/main/java/com/dhbw/tutorsystem/security/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // correspond to REST
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/ping").permitAll()
+                .antMatchers("/tutorial").permitAll()
                 .antMatchers("/authentication/**").permitAll()
                 .antMatchers("/v3/api-docs/**").permitAll()// TODO: in the future .hasRole(ERole.ROLE_ADMIN.name())
                 .antMatchers("/swagger-ui/**").permitAll()// TODO: in the future.hasRole(ERole.ROLE_ADMIN.name())
