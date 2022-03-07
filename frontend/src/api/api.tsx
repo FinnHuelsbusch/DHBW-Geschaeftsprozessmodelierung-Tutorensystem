ef2b3af336d1c0aeae0e727e09d8860a8e58b328
Robin <rbae@web.de>
2022-03-07 10:48:51 +0100

    add .env file

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -4,7 +4,7 @@ import { ErrorCode, RequestError } from '../types/RequestError';
 import { TutorialOffer } from '../types/Tutorial';
 import { User } from '../types/User';
 
-const backendUrl = 'http://localhost:8080';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
     baseURL: backendUrl,
