fe8374fcdd3a6569b4768ae09ae778f9decccf78
FinnHuelsbusch <f.huelsbusch@outlook.de>
2022-03-09 17:49:20 +0100

    cleanup

--- backend/src/main/java/com/dhbw/tutorsystem/user/dto/UserWithEmailAndName.java
+++ backend/src/main/java/com/dhbw/tutorsystem/user/dto/UserWithEmailAndName.java
@@ -1,10 +1,6 @@
 package com.dhbw.tutorsystem.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 public class UserWithEmailAndName{
