a9082703e5cb323d8df5a2f47b7f77e18b3d691d
I516994 <johannes.knoll@sap.com>
2022-03-14 17:21:37 +0100

    Created mockup data

---
+++ backend/src/main/java/com/dhbw/tutorsystem/tutorialRequest/TutorialRequestRepository.java
@@ -0,0 +1,8 @@
+package com.dhbw.tutorsystem.tutorialRequest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRequestRepository extends JpaRepository<TutorialRequest,Integer>{
+    
+}
+
