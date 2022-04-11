076f76b4a6e7d2d122464ee12637c1e1894cdbb1
Robin <rbae@web.de>
2022-04-11 15:43:36 +0200

    remove unnecessary state

--- frontend/src/components/inputs/PasswordInput.tsx
+++ frontend/src/components/inputs/PasswordInput.tsx
@@ -14,10 +14,7 @@ const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g;
 
 const PasswordInput: React.FC<Props> = ({ disabled = false, customLabel, withConfirmForm, noRegexValidation = false }) => {
 
-
-
-    const [passwordValidationStatus, setPasswordValidationStatus] = useState<ValidateStatus|undefined>();
-    const [passwordConfirmValidationStatus, setPasswordConfirmValidationStatus] = useState<ValidateStatus|undefined>();
+    const [passwordValidationStatus, setPasswordValidationStatus] = useState<ValidateStatus | undefined>();
 
     const passwordsMatch = (password: string, passwordConfirm: string): boolean => {
         return (
@@ -43,14 +40,11 @@ const PasswordInput: React.FC<Props> = ({ disabled = false, customLabel, withCon
         if (!withConfirmForm) return;
         const { password, passwordConfirm } = withConfirmForm.getFieldsValue();
         if (!passwordsMatch(password, passwordConfirm)) {
-            setPasswordConfirmValidationStatus('error');
             return Promise.reject(validateMessages.pattern.password.notMatching);
         }
         if (!noRegexValidation && !value.match(passwordRegex)) {
-            setPasswordConfirmValidationStatus('error');
             return Promise.reject(validateMessages.pattern.password.notFulfillingRegex);
         }
-        setPasswordConfirmValidationStatus(undefined);
         return Promise.resolve();
     };
 
